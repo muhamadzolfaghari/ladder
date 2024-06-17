a80950b0e9abe4e38fa76486b50eea31bebd6321
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-17 10:49:28 +0330

    feat (root folder): add lib and some componenets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import MyDialog from "./data/page";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <MyDialog />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
