94b31bbab697661b239fb720a330d7c7d21d4c44
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-17 23:55:07 +0330

    feat (api): add details

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -1,3 +1,51 @@
+import axios from "axios";
+
 export async function GET() {
-  return Response.json({ data: 1 });
+  return Response.json(run());
+}
+
+async function run() {
+  const apiKey = "AIzaSyBguY4FmP090KrqaSuLt95Emrs3y8_j260";
+  const prompt = "Write a story about a magic backpack.";
+
+  const request = {
+    contents: [
+      {
+        parts: [
+          {
+            text: prompt,
+          },
+        ],
+      },
+    ],
+    safetySettings: [
+      {
+        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold: "BLOCK_ONLY_HIGH",
+      },
+    ],
+    generationConfig: {
+      stopSequences: ["Title"],
+      temperature: 1.0,
+      maxOutputTokens: 800,
+      topP: 0.8,
+      topK: 10,
+    },
+  };
+
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+
+  try {
+    const response = await axios.post(url, request, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (e) {
+    console.log(e);
+    
+    return e;
+  }
 }
