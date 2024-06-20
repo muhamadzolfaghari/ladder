a024326cccd07539cb9c72036ca5dd8b87487c2a
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-20 23:33:40 +0330

    fix (prompt page): resolve conflict

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import nextPWA from "next-pwa";
 
 const withPWA = nextPWA({
   dest: "public",
+  sourcemap: false
 });
 
 const nextConfig = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 import { Button } from "@mui/material";
+=======
+>>>>>>> master
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+<<<<<<< HEAD
       <Link href="/Prompt1">
         <Button>Get Started</Button>
       </Link>
+=======
+<Link href="/getStart">start</Link>
+>>>>>>> master
     </>
   );
 }
