c7622523f691fd0fcec453cc8a3f7b2a38dad1eb
ZohreScript <z.omidi74@gmail.com>
2024-06-21 14:50:01 +0330

    fix(correct image):add image to public/images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { Button } from "@mui/material";
-=======
->>>>>>> master
-=======
->>>>>>> master
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <Link href="/Prompt1">
         <Button>Get Started</Button>
       </Link>
-=======
-<Link href="/getStart">start</Link>
->>>>>>> master
-=======
-<Link href="/getStart">start</Link>
->>>>>>> master
+
+<Link href="/get-start">start</Link>
+
     </>
   );
 }
