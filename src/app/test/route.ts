cd3bf69de4acb287b45bba5b6d103881c9634286
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-21 22:13:07 +0330

    feat (api): updating API

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -1,33 +1,46 @@
 import axios from "axios";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(request: NextRequest) {
   const url =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCWxoLsLt5_xn7d4QtCcIZmSshzdTiNVNc";
 
-  const data = {
-    contents: [
-      {
-        parts: [
-          {
-            text: "Explain how AI works",
-          },
-        ],
-      },
-    ],
-  };
-
   try {
+    const { prompt } = await request.json();
+
+    if (!prompt) {
+      return NextResponse.json(
+        { error: "Prompt is required" },
+        { status: 400 }
+      );
+    }
+
+    const data = {
+      contents: [
+        {
+          parts: [
+            {
+              text: prompt,
+            },
+          ],
+        },
+      ],
+    };
+
     const response = await axios.post(url, data, {
       headers: {
         "Content-Type": "application/json",
       },
     });
+
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
+    console.error("Error:", error);
     return NextResponse.json(
       {
-        error: (error as { response: { data: string } }).response.data,
+        error:
+          (error as { response: { data: string } })?.response?.data ||
+          "An error occurred",
       },
       { status: 500 }
     );
