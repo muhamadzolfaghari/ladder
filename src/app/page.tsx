0c4ddf0b03bb22789f577efd4548968ee97e0f9f
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-21 22:36:50 +0330

    feat (gemini api): make api available to access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import { Button } from "@mui/material";
+import axios from "axios";
 
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    axios
+      .post(process.env.NEXT_PUBLIC_API_URL + "/test", {
+        data: { prompt: "what is the meaning of life?" },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
+
   return (
     <>
       <Link href="/Prompt1">
