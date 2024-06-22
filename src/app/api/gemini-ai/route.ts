de023fd72ad78778fc19f07bd4540b8900963ec8
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-22 22:11:08 +0330

    api

--- src/app/api/gemini-ai/route.ts
+++ src/app/api/gemini-ai/route.ts
@@ -1,23 +1,28 @@
-import { applyCors } from "@/lib/utilities/applyCors";
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export function OPTIONS() {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+
+  return NextResponse.json({}, { headers });
+}
+
+export async function POST(request: NextRequest) {
   const url =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCWxoLsLt5_xn7d4QtCcIZmSshzdTiNVNc";
 
-  console.log(res);
-
-  if (applyCors(req, res)) {
-    return;
-  }
-
   try {
-    const { prompt } = req.body;
+    const { prompt } = await request.json();
 
     if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
+      return NextResponse.json(
+        { error: "Prompt is required" },
+        { status: 400 }
+      );
     }
 
     const data = {
@@ -39,10 +44,13 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
     console.error("Error:", error);
-    return res.status(500).json({
-      error:
-        (error as { response: { data: string } })?.response?.data ||
-        "An error occurred",
-    });
+    return NextResponse.json(
+      {
+        error:
+          (error as { response: { data: string } })?.response?.data ||
+          "An error occurred",
+      },
+      { status: 500 }
+    );
   }
 }
--- src/lib/utilities/applyCors.ts
+++ src/lib/utilities/applyCors.ts
@@ -1,15 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export function applyCors(req: NextApiRequest, res: NextApiResponse): boolean {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+export function applyCors(req) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
 
   if (req.method === "OPTIONS") {
     // Handle preflight request
-    res.status(200).json({ message: "CORS preflight response" });
-    return true;
+    return NextResponse.json({}, { headers });
   }
 
-  return false;
+  return NextResponse.next();
 }
