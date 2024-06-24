f73bb71374513aef02b58c03570a7d0c3714d10f
ZohreScript <z.omidi74@gmail.com>
2024-06-24 12:03:24 +0330

    feat(loading page):add loading

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -1,32 +1,7 @@
-import React from 'react';
-import { Container, Box, Typography, CircularProgress } from '@mui/material';
-import Image from 'next/image';
-
-const styles = {
-  loader: {
-    width: '8px',
-    height: '48px',
-    display: 'block',
-    margin: 'auto',
-    position: 'relative',
-    borderRadius: '4px',
-    boxSizing: 'border-box',
-    animation: 'animloader 1s linear infinite alternate',
-  },
-  '@global': {
-    '@keyframes animloader': {
-      '0%': {
-        boxShadow: '20px 0 rgba(255, 255, 255, 0.25), 40px 0 white, 60px 0 white',
-      },
-      '50%': {
-        boxShadow: '20px 0 white, 40px 0 rgba(255, 255, 255, 0.25), 60px 0 white',
-      },
-      '100%': {
-        boxShadow: '20px 0 white, 40px 0 white, 60px 0 rgba(255, 255, 255, 0.25)',
-      },
-    },
-  },
-};
+import React from "react";
+import { Container, Box, Typography } from "@mui/material";
+import Image from "next/image";
+import SpinnerLoader from "@/components/SpinnerLoader";
 
 export default function Page() {
   return (
@@ -36,12 +11,12 @@ export default function Page() {
         alignItems="center"
         p={1}
         height="100vh"
-        sx={{ mt: 2, display: { xs: 'flex', md: 'none' } }}
+        sx={{ mt: 2, display: { xs: "flex", md: "none" } }}
       >
         <Image
           width={320}
           height={306}
-          style={{ margin: '1rem' }}
+          style={{ margin: "1rem" }}
           src="/Images/loading.svg"
           alt="Wait screen illustration"
         />
@@ -51,20 +26,14 @@ export default function Page() {
         <Typography variant="body1" align="center" mt={3}>
           We are working on your Ladder, It will be ready soon :)
         </Typography>
-        <Box 
-          display="flex" 
-          justifyContent="center" 
-          alignItems="center" 
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
           flexGrow={1}
-          
         >
-          <CircularProgress  />
+          <SpinnerLoader />
         </Box>
-
-        {/* <Box sx={styles.loader} bgcolor='#424940' >
-        <Typography variant="body1" align="center" mt={2}>
-          Loading...
-        </Typography></Box> */}
       </Box>
     </Container>
   );
