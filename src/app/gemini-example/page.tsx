7109f2e535a57fc3263d924806752cce289c960d
muhamad <muhamad.zolfaghari@gmail.com>
2024-06-25 14:29:56 +0330

    feat(api): make request by POST method

--- src/app/gemini-example/page.tsx
+++ src/app/gemini-example/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 console.log(process.env.NEXT_PUBLIC_API_URL, "base api url");
 
@@ -8,26 +9,8 @@ const GeminiExample = () => {
   useEffect(() => {
     const url = "https://ladder-nu.vercel.app/api/gemini-ai";
 
-
-    // axios
-    //   .post(
-    //     url,
-    //     {
-    //       prompt: "tell me hi",
-    //     },
-    //     // { headers: { "Content-Type": "application/json" } }
-    //   )
-    //   .then((res) => {
-    //     console.log(res);
-    //   });
-
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        prompt: "give me a roadmap for frontend developer in 3 weeks",
-      }),
-    }).then(res => {
-      console.log(res)
+    axios.get(url).then((res) => {
+      console.log(res);
     });
   }, []);
 
