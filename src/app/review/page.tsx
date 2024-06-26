7fbcd10a978c7fc3f57277a645d1602703a69824
itsnooshin <nooshindev@gmail.com>
2024-06-26 13:58:26 +0330

    refactor(prompt-pages):remove 'hidden' for prompt-page 3

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -8,9 +8,9 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <Box sx={{ mt: 4, display: { md: "none" }, px: "1rem" }}>
+      <Box sx={{ mt: 4, px: "1rem" }}>
         <StepBarReview />
-         <ReviewPrompt />
+        <ReviewPrompt />
       </Box>
     </>
   );
