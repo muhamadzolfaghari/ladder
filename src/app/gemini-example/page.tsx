a35495018c04eb47ab61f24a5eac4cb1d2583462
Mohammad <muhamad.zolfaghari@gmail.com>
2024-07-03 00:20:37 +0330

    fix(page): remove some useless code

--- src/app/gemini-example/page.tsx
+++ src/app/gemini-example/page.tsx
@@ -1,111 +1,94 @@
 "use client";
 
 import React, {
-  ChangeEvent,
-  FormEvent,
-  useCallback,
-  useEffect,
-  useState,
+    ChangeEvent,
+    FormEvent,
+    useCallback,
+    useEffect,
+    useState,
 } from "react";
 import axios from "axios";
 import {
-  Button,
-  Container,
-  Divider,
-  Stack,
-  TextField,
-  Typography,
+    Button,
+    Container,
+    Divider,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
 
 const GeminiExample = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [prompt, setPrompt] = useState<string>();
-  const [data, setData] = useState<string>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [prompt, setPrompt] = useState<string>();
+    const [data, setData] = useState<string>();
 
-  const tryGetItemFromLocalStorage = useCallback(() => {
-    try {
-      localStorage.getItem("key");
-    } catch (e) {
-      console.log();
-    }
-  }, []);
 
-  useEffect(() => {
-    if (typeof window === "undefined") {
-      console.log("sfd")
-      return;
+    function handlePromptChange(
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ): void {
+        setData(undefined);
+        setPrompt(event.target.value);
     }
 
-    localStorage.getItem("key");
-    console.log(window)
-  }, [tryGetItemFromLocalStorage]);
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+        const url = "https://ladder-nu.vercel.app/api/gemini-ai";
 
-  function handlePromptChange(
-      event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ): void {
-    setData(undefined);
-    setPrompt(event.target.value);
-  }
+        const data = prompt
+            ?.split("\n")
+            .map((line) => line.split(":").map((x) => x.trim()))
+            .reduce((prev, [key, value]) => ({...prev, [key]: value}), {});
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    const url = "https://ladder-nu.vercel.app/api/gemini-ai";
+        // const data = {
+        //   // field_of_study: "UIUX",
+        //   // goal: "Become an expert",
+        //   // current_level: "Basic",
+        //   // time_commitment: "3 hours a day",
+        //   // preferred_learning_style: "Videos",
+        //   // learning_pace: "Fast",
+        //   // resources_available: "$1000",
+        //   // preferred_tools_and_platforms: "Figma",
+        //   // language: "English",
+        // };
+        setIsLoading(true);
 
-    const data = prompt
-        ?.split("\n")
-        .map((line) => line.split(":").map((x) => x.trim()))
-        .reduce((prev, [key, value]) => ({ ...prev, [key]: value }), {});
+        axios
+            .post(url, data)
+            .then((res) => {
+                setData(res.data.data);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
 
-    // const data = {
-    //   // field_of_study: "UIUX",
-    //   // goal: "Become an expert",
-    //   // current_level: "Basic",
-    //   // time_commitment: "3 hours a day",
-    //   // preferred_learning_style: "Videos",
-    //   // learning_pace: "Fast",
-    //   // resources_available: "$1000",
-    //   // preferred_tools_and_platforms: "Figma",
-    //   // language: "English",
-    // };
-    setIsLoading(true);
-
-    axios
-        .post(url, data)
-        .then((res) => {
-          setData(res.data.data);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-  }
-
-  return (
-      <Container>
-        <form onSubmit={handleSubmit}>
-          <Typography variant={"h1"}>Gemini Example</Typography>
-          <Stack direction={"row"} m={"auto"}>
-            <TextField
-                fullWidth
-                sx={{ mr: 2 }}
-                label={"prompt"}
-                value={prompt}
-                multiline
-                minRows={2}
-                onChange={handlePromptChange}
-            />
-            <Button
-                variant={"contained"}
-                disabled={!prompt || isLoading}
-                type={"submit"}
-            >
-              submit
-            </Button>
-          </Stack>
-          <Divider sx={{ mt: 2, mb: 2 }} />
-          <Typography component={"pre"}>{data}</Typography>
-        </form>
-      </Container>
-  );
+    return (
+        <Container>
+            <form onSubmit={handleSubmit}>
+                <Typography variant={"h1"}>Gemini Example</Typography>
+                <Stack direction={"row"} m={"auto"}>
+                    <TextField
+                        fullWidth
+                        sx={{mr: 2}}
+                        label={"prompt"}
+                        value={prompt}
+                        multiline
+                        minRows={2}
+                        onChange={handlePromptChange}
+                    />
+                    <Button
+                        variant={"contained"}
+                        disabled={!prompt || isLoading}
+                        type={"submit"}
+                    >
+                        submit
+                    </Button>
+                </Stack>
+                <Divider sx={{mt: 2, mb: 2}}/>
+                <Typography component={"pre"}>{data}</Typography>
+            </form>
+        </Container>
+    );
 };
 
 export default GeminiExample;
