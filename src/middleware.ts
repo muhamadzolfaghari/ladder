781da593ebc3c1ad2301a0e231fced99d058df27
Mohammad <muhamad.zolfaghari@gmail.com>
2024-07-21 22:58:24 +0330

    fix(middleware): ignore some urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,15 @@ export const config = {
   matcher: "/api/:function*",
 };
 
+const FORBIDDEN_ROUTES = ["auth/google", "auth/callback", "auth/status"];
+
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
-  if (!url.href.endsWith("auth/google") && !isAuthenticated(request)) {
+  if (
+    !FORBIDDEN_ROUTES.every((route) => url.href.endsWith(route)) &&
+    !isAuthenticated(request)
+  ) {
     return NextResponse.json(
       { message: "request is unauthorized" },
       { status: 401 },
