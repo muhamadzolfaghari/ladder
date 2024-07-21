e81299492509269b9987723f5514dbb300f27384
Mohammad <muhamad.zolfaghari@gmail.com>
2024-07-22 00:33:14 +0330

    test

--- src/components/UserGreetText.tsx
+++ src/components/UserGreetText.tsx
@@ -1,19 +1,18 @@
 "use client";
-import { createClient } from "@/lib/utilities/supabase/client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const UserGreetText = () => {
   const [user, setUser] = useState<any>(null);
-  const supabase = createClient();
-  useEffect(() => {
-    const fetchUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      setUser(user);
-    };
-    fetchUser();
-  }, []);
+  // const supabase = createClient();
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const {
+  //       data: { user },
+  //     } = await supabase.auth.getUser();
+  //     setUser(user);
+  //   };
+  //   fetchUser();
+  // }, []);
 
   return (
     <>{user !== null && <p>{user.user_metadata.full_name ?? "user"}</p>}</>
