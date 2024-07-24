d8287cf0f7c9e93e052a40373c3a6e8d96e296f8
Mohammad <muhamad.zolfaghari@gmail.com>
2024-07-25 00:09:01 +0330

    fix(auth): resolve a problem

--- src/auth.ts
+++ src/auth.ts
@@ -2,10 +2,5 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
+  providers: [Google],
 });
