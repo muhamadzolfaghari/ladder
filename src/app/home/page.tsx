7133badbb01a6ab6affeca896dc4f6a2ed368017
Mohammad Zolfaghari <muhamad.zolfaghari@gmail.com>
2024-08-05 02:27:55 +0530

    Revert "dsffs"

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,19 +3,9 @@ import LadderOverview from "@/components/LadderOverview";
 import NavigationBottom from "@/components/NavigationBottom";
 import QuoteDay from "@/components/QuoteDay";
 import TaskTrackerForDay from "@/components/TaskTrackerForDay";
-import { fetchVisitorStatus, useVisitorStatus } from "@/hooks/VisitorStatus";
 import { Box, Container } from "@mui/material";
 
-const page = async () => {
-  let isFirstVisit: boolean;
-
-  try {
-    const res = await fetchVisitorStatus();
-    isFirstVisit = res.is_first_visit;
-  } catch {
-    return <div>Error</div>;
-  }
-
+const page = () => {
   return (
     <Container sx={{ pt: 2 }}>
       <HeaderHomePage />
