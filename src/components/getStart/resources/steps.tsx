a31c67fbffd5cadd012c28a4f8d35589e79f61ad
mohammad.z <mohamamd.z@780.team>
2024-08-08 00:01:06 +0330

    fix some fiels

--- src/components/getStart/resources/steps.tsx
+++ src/components/getStart/resources/steps.tsx
@@ -7,19 +7,19 @@ interface Step {
 
 export const STEPS: Step[] = [
   {
-    imageSrc: "/Images/startstep1.svg",
+    imageSrc: "/images/startstep1.svg",
     title: "What is Ladder?",
     description:
       "Ladder is your roadmap for learning anything and tracking your progress. Our AI crafts a learning path tailored to your conditions and preferences.",
   },
   {
-    imageSrc: "/Images/startstep2.svg",
+    imageSrc: "/images/startstep2.svg",
     title: "How It Works?",
     description:
       "Simply provide an accurate and comprehensive prompt for our AI model, Gemini. It will then create a customized Ladder just for you.",
   },
   {
-    imageSrc: "/Images/startstep3.svg",
+    imageSrc: "/images/startstep3.svg",
     title: "Let’s Kick Off",
     description:
       "Create your first Ladder and start your journey. Whether it's small daily tasks or major milestones, Ladder supports you every step of the way.",
