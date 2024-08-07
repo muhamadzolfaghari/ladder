8ebd86805dcc6cc7f68996bceed3eebca763d5a7
mohammad.z <mohamamd.z@780.team>
2024-08-08 00:59:46 +0330

    chor(config): add svgr to project

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   headers: async () => {
     return [
       {
