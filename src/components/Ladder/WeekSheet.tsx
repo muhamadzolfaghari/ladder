7b4a5f9706d4521b531e138d7f160134bb4df92a
ZohreScript <z.omidi74@gmail.com>
2024-08-11 17:40:00 +0330

    fix(fix-week-sheet): implement week-sheet UI

--- src/components/Ladder/WeekSheet.tsx
+++ src/components/Ladder/WeekSheet.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useState } from "react";
@@ -22,85 +21,88 @@ import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import { Timeline, Assignment, BarChart, Margin } from "@mui/icons-material";
 import BottomNav from "@/components/BottomNav";
 import theme from "@/lib/resources/theme";
-export default function WeekSheet() {
-    const [expanded, setExpanded] = useState<string | false>(false);
 
-    return (
-      <Container
-        maxWidth="sm"
+
+export default function WeekSheet() {
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Box
+        p={2}
+        my={3}
+        borderRadius={2}
+        sx={{ bgcolor: theme.palette.primary.main }}
+      >
+        <Typography variant="h3">Phase 1, Week 4</Typography>
+      </Box>
+      
+      <Accordion>
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white !important",
+
+            "& .MuiTypography-root": {
+              color: "#2D322C",
+            },
+          }}
+          expandIcon={
+            expanded === "panel1" ? (
+              <ArrowDropDownIcon fontSize="large" />
+            ) : (
+              <ArrowRightIcon fontSize="large" />
+            )
+          }
+        >
+          <Typography>
+            {`Day ${1} ${5 ? "Weekdays" : "Weekends"} (${5 ? "2 hours/day" : "4 hours/day"})`}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="1 hour: Online course (HTML, CSS, JavaScript basics) – Codecademy or Udemy."
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="1 hour: Online course (HTML, CSS, JavaScript basics) – Codecademy or Udemy."
+            />
+          </>
+        </AccordionDetails>
+      </Accordion>
+
+      <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          height: "100vh",
+          flexGrow: 1,
+          justifyContent: "start",
         }}
       >
-        <Box
-          p={2}
-          my={3}
-          borderRadius={2}
-          sx={{ bgcolor: theme.palette.primary.main }}
-        >
-          <Typography variant="h3">Phase 1, Week 4</Typography>
-        </Box>
-        <Accordion>
-          <AccordionSummary
-            sx={{
-              backgroundColor: "white !important",
-  
-              "& .MuiTypography-root": {
-                color: "#2D322C",
-              },
-            }}
-            expandIcon={
-              expanded === "panel1" ? (
-                <ArrowDropDownIcon fontSize="large" />
-              ) : (
-                <ArrowRightIcon fontSize="large" />
-              )
-            }
-          >
-            <Typography>
-              {`Day ${1} ${5 ? "Weekdays" : "Weekends"} (${5 ? "2 hours/day" : "4 hours/day"})`}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="1 hour: Online course (HTML, CSS, JavaScript basics) – Codecademy or Udemy."
-              />
-              <FormControlLabel
-                control={<Checkbox />}
-                label="1 hour: Online course (HTML, CSS, JavaScript basics) – Codecademy or Udemy."
-              />
-            </>
-          </AccordionDetails>
-        </Accordion>
-  
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            flexGrow: 1,
-            justifyContent: "start",
-          }}
-        >
-          <Button variant="contained" color="success" fullWidth>
-            Review Your Performance
-          </Button>
-        </Box>
-  
-        <Box
-          sx={{
-            display: "flex",
-            bottom: 0,
-            left: 0,
-            right: 0,
-            flexDirection: "column",
-          }}
-        >
-          <BottomNav />
-        </Box>
-      </Container>
-    );
+        <Button variant="contained" color="success" fullWidth>
+          Review Your Performance
+        </Button>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          flexDirection: "column",
+        }}
+      >
+        <BottomNav />
+      </Box>
+    </Container>
+  );
 }
