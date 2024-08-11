f6309da24d48cac31f28f8cd1a1ca3d8e2c39abd
ZohreScript <z.omidi74@gmail.com>
2024-08-11 18:56:18 +0330

    fix(fix/review-performance): review performance 2  UI

--- src/app/week-sheet/page.tsx
+++ src/app/week-sheet/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import WeekSheet from "@/components/Ladder/WeekSheet";
 import ReviewPerformance1 from "@/components/Ladder/ReviewPerformance/ReviewPerformance1";
+import ReviewPerformance2 from "@/components/Ladder/ReviewPerformance/ReviewPerformance2";
 
 export default function Page() {
-  return   <WeekSheet/>
+  return   <ReviewPerformance2/>
   // <WeekSheet />;
 }
